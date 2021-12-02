13b536c112fa1f0df1baf9055f7fecc0f68343b5
Jacob Daleske <60079607+jdaleske13@users.noreply.github.com>
2021-12-01 18:59:05 -0600

    added secure login
    user can no longer update drinks without being logged in

--- routes/drinks.js
+++ routes/drinks.js
@@ -2,11 +2,19 @@ var express = require('express');
 const drinks_controller = require('../controllers/drinks');
 var router = express.Router();
 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+
 /* GET home page. */
 router.get('/', drinks_controller.drink_view_all_Page);
 router.get('/detail', drinks_controller.drink_view_one_Page);
 router.get('/create', drinks_controller.drink_create_Page);
-router.get('/update', drinks_controller.drink_update_Page);
+router.get('/update', secured, drinks_controller.drink_update_Page);
 router.get('/delete', drinks_controller.drink_delete_Page);
 
 module.exports = router;
