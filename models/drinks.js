e738149a814a1fe6dc3899be66ccc98ae73a66b4
Jacob Daleske <60079607+jdaleske13@users.noreply.github.com>
2021-12-01 19:15:25 -0600

    added validator
    added validator to the drink cost

--- models/drinks.js
+++ models/drinks.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const drinkSchema =mongoose.Schema({
     drink_name: String,
     drink_flavor: String,
-    drink_cost: Number
+    drink_cost:{
+        type: Number,
+        min: 0,
+        max: 100}
+
 });
 
 module.exports = mongoose.model("drinks", drinkSchema)
